b38df8457a02659ed73aa0a1dfa36ae16da80708
Kelvin Lu <kelvinlu@nbnco.com.au>
2017-07-04 22:37:41 +1000

    workable simple PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,38 @@
 #include "PID.h"
 
-using namespace std;
-
 /*
-* TODO: Complete the PID class.
-*/
+ * TODO: Complete the PID class.
+ */
 
-PID::PID() {}
+PID::PID() {
+}
 
-PID::~PID() {}
+PID::~PID() {
+}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+
+    this->Kd = Kd;
+    this->Ki = Ki;
+    this->Kp = Kp;
+
+    prev_cte = 0;
 }
 
 void PID::UpdateError(double cte) {
+
+    p_error = cte;
+    d_error = cte - prev_cte;
+    i_error *= 0.99;
+    i_error += cte;
+
+    prev_cte = cte;
 }
 
 double PID::TotalError() {
+    return Kp * p_error + Ki * i_error + Kd * d_error;
 }
 
